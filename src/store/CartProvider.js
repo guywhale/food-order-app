1d52a043c67d95e0a496f3c93b9001014f8de202
Guy Whale <guywhale@gmail.com>
2022-08-01 21:01:54 +0100

    Add a reducer to the cart context to manage state changes for new items added/remove and subsequent adjustment of total amount

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,12 +1,46 @@
+import { useReducer } from 'react';
 import CartContext from './cart-context';
 
+const defaultCartState = {
+	items: [],
+	totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+	if (action.type === 'ADD_ITEM') {
+		/**
+		 * You must always create new arrays via .concat() as changes/merges
+		 * to existing arrays in state will not be registered by React
+		 */
+		const updatedItems = state.items.concat(action.item);
+		const updatedTotalAmount =
+			state.totalAmount + action.item.amount * action.item.price;
+
+		return {
+			items: updatedItems,
+			totalAmount: updatedTotalAmount,
+		};
+	}
+
+	return defaultCartState;
+};
+
 const CartProvider = (props) => {
-	const addItemToCartHandler = (item) => {};
-	const removeItemFromCartHandler = (id) => {};
+	const [cartState, dispatchCartAction] = useReducer(
+		cartReducer,
+		defaultCartState
+	);
+
+	const addItemToCartHandler = (item) => {
+		dispatchCartAction({ type: 'ADD_ITEM', item: item });
+	};
+	const removeItemFromCartHandler = (id) => {
+		dispatchCartAction({ type: 'REMOVE_ITEM', id: id });
+	};
 
 	const cartContext = {
-		items: [],
-		totalAmount: 0,
+		items: cartState.items,
+		totalAmount: cartState.totalAmount,
 		addItem: addItemToCartHandler,
 		removeItems: removeItemFromCartHandler,
 	};
