e3c3d2eae6c272ab54f71277a3ab47ca2074386a
Guy Whale <guywhale@gmail.com>
2022-08-01 20:43:45 +0100

    Add total number of items in cart to header cart button badge

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+	const cartContext = useContext(CartContext);
+	const noOfItemsInCart = cartContext.items.reduce((currentNumber, item) => {
+		return currentNumber + item.amount;
+	}, 0);
+
 	return (
 		<button className={classes.button} {...props}>
 			<span className={classes.icon}>
 				<CartIcon />
 			</span>
 			<span>{props.children}</span>
-			<span className={classes.badge}>3</span>
+			<span className={classes.badge}>{noOfItemsInCart}</span>
 		</button>
 	);
 };
