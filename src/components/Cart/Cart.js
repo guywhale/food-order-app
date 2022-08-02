61b81c688cdc5eb93cab07ad8f32ce2cf6d1909d
Guy Whale <guywhale@gmail.com>
2022-08-02 12:14:01 +0100

    Adjust cart reducer logic so that when an new item is added, it checks whether that item exists and only updates the quantity of the existing item if true. If false, it simply adds a new item.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,33 @@
+import { useContext } from 'react';
 import classes from './Cart.module.css';
 import Modal from '../UI/Modal';
+import CartContext from '../../store/cart-context';
+import CartItem from './CartItem/CartItem';
 
 const Cart = (props) => {
+	const cartContext = useContext(CartContext);
+	const totalAmount = `$${cartContext.totalAmount.toFixed(2)}`;
+	const hasItems = cartContext.items.length > 0;
+
+	const cartItemRemoveHandler = (id) => {};
+
+	const cartItemAddHandler = (item) => {
+		cartContext.addItem({ ...item, amount: 1 });
+	};
+
 	const cartItems = (
 		<ul className={classes['cart-items']}>
-			{[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => {
-				return <li key={item.id}>{item.name}</li>;
+			{cartContext.items.map((item) => {
+				return (
+					<CartItem
+						key={item.id}
+						name={item.name}
+						price={item.price}
+						amount={item.amount}
+						onRemove={cartItemRemoveHandler.bind(null, item.id)}
+						onAdd={cartItemAddHandler.bind(null, item)}
+					/>
+				);
 			})}
 		</ul>
 	);
@@ -15,13 +37,13 @@ const Cart = (props) => {
 			{cartItems}
 			<div className={classes.total}>
 				<span>Total Amount</span>
-				<span>35.62</span>
+				<span>{totalAmount}</span>
 			</div>
 			<div className={classes.actions}>
 				<button className={classes['button--alt']} onClick={props.onHideCart}>
 					Close
 				</button>
-				<button className={classes.button}>Order</button>
+				{hasItems && <button className={classes.button}>Order</button>}
 			</div>
 		</Modal>
 	);
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,13 +8,55 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
 	if (action.type === 'ADD_ITEM') {
-		/**
-		 * You must always create new arrays via .concat() as changes/merges
-		 * to existing arrays in state will not be registered by React
-		 */
-		const updatedItems = state.items.concat(action.item);
 		const updatedTotalAmount =
 			state.totalAmount + action.item.amount * action.item.price;
+
+		/**
+		 * Check if current item being added exists in current list of
+		 * items. If so, find the index of existing item in state.items
+		 * array.
+		 */
+		const existingCartItemIndex = state.items.findIndex((item) => {
+			return item.id === action.item.id;
+		});
+
+		const existingCartItem = state.items[existingCartItemIndex];
+
+		let updatedItems;
+
+		/**
+		 * If the item being added already exists...
+		 */
+		if (existingCartItem) {
+			/** ...create a new object containing the existing item data
+			 * and add the amount of the new items to existing amount...
+			 */
+			const updatedItem = {
+				...existingCartItem,
+				amount: existingCartItem.amount + action.item.amount,
+			};
+
+			/** ...create a new array with all the existing items
+			 * (you must always create new arrays as changes/merges
+			 * to existing arrays in state will not be registered by React)...
+			 *
+			 */
+			updatedItems = [...state.items];
+
+			/** ...swap the existing item for the new updated item using the
+			 * new updated items array and the existing item index number
+			 * found earlier.
+			 */
+			updatedItems[existingCartItemIndex] = updatedItem;
+		} else {
+			/**
+			 * Else if new item doesn't alreay exist, simply add it to the
+			 * existing list of items (you must always create new arrays via
+			 * .concat() as changes/merges to existing arrays in state will
+			 * not be registered by React).
+			 */
+			updatedItems = state.items.concat(action.item);
+		}
 
 		return {
 			items: updatedItems,
