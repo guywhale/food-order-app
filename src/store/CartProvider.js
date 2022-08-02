b61dfdf4f31bdd9feab76259513a3e509fcbfecc
Guy Whale <guywhale@gmail.com>
2022-08-02 12:52:58 +0100

    Set remove item handler so that cart state is updated when an item is removed via clicking the remove item button in the cart. Also, if remove item handler is triggered when there is only one item of a kind left, the item is completely removed from the cart (items list)

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,6 +8,11 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
 	if (action.type === 'ADD_ITEM') {
+		/**
+		 * Update total amount by multiplying the amount of the new
+		 * item by its price and adding it to the existing total
+		 * amount
+		 */
 		const updatedTotalAmount =
 			state.totalAmount + action.item.amount * action.item.price;
 
@@ -64,6 +69,67 @@ const cartReducer = (state, action) => {
 		};
 	}
 
+	if (action.type === 'REMOVE_ITEM') {
+		/**
+		 * Find the index of the existing item to be removed in
+		 * state.items array.
+		 */
+		const existingCartItemIndex = state.items.findIndex((item) => {
+			return item.id === action.id;
+		});
+
+		const existingCartItem = state.items[existingCartItemIndex];
+
+		/**
+		 * Update total amount by subtracting the price of the removed item
+		 * from the existing total amount. The price of only one item is needed
+		 * as only one item will ever be removed at a time, there is no option
+		 * to remove batches of items in the app.
+		 */
+		const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+
+		let updatedItems;
+
+		/**
+		 * Check if the current amount of the item to be removed is 1.
+		 */
+		if (existingCartItem.amount === 1) {
+			/**
+			 * If there is only 1 item left, remove item entirely from list.
+			 * Create new array (so React can see it) that filters out item to be
+			 * removed from existing list of item using the ID.
+			 */
+			updatedItems = state.items.filter((item) => {
+				return item.id !== action.id;
+			});
+		} else {
+			/** If the current amount of the item to be removed is > 1,
+			 * first create a new item object and subtract the existing item's
+			 * amount by 1 (only 1 item can be removed at a time)
+			 */
+			const updatedItem = {
+				...existingCartItem,
+				amount: existingCartItem.amount - 1,
+			};
+
+			/**
+			 * Then create a new array with all the existing items in so that React
+			 * can detect changes.
+			 */
+			updatedItems = [...state.items];
+			/**
+			 * The use the existing item index found earlier to update the existing
+			 * item with the new updated item object containing the correct amount.
+			 */
+			updatedItems[existingCartItemIndex] = updatedItem;
+		}
+
+		return {
+			items: updatedItems,
+			totalAmount: updatedTotalAmount,
+		};
+	}
+
 	return defaultCartState;
 };
 
@@ -84,7 +150,7 @@ const CartProvider = (props) => {
 		items: cartState.items,
 		totalAmount: cartState.totalAmount,
 		addItem: addItemToCartHandler,
-		removeItems: removeItemFromCartHandler,
+		removeItem: removeItemFromCartHandler,
 	};
 
 	return (
